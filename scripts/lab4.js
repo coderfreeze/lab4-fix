db163b0b2b95294619a632ed25cb80c4bfa08be8
coderfreeze <vijaymastern@gmail.com>
2025-04-26 03:46:51 -0700

    fixed lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,9 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof add !== 'boolean') {
+        return false;
+    }
     if (add) {
         let result = 0;
 
@@ -14,7 +17,7 @@ function sumValues(num1, num2, add) {
         return result;
     }
     else {
-        return;
+        return false;
     }
 }
 
@@ -25,7 +28,7 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    if (prices.length === 0) {
+    if (prices.length === 0 || typeof discount !== 'number' || !Array.isArray(prices)) {
         return false;
     }
     const discounted = []
